7e7c01c75ce20a95b1416c3ee4d8c5c954813648
Pedro Cunha <p.eduardo.cunha@gmail.com>
2024-08-16 14:41:43 +0100

    adds nvim-treesitter

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -63,6 +63,7 @@ return require('packer').startup(function(use)
   use 'L3MON4D3/LuaSnip'
   use 'saadparwaiz1/cmp_luasnip'
 
+  use 'nvim-treesitter/nvim-treesitter'
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
